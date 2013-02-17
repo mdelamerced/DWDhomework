959d010e018ccb41054bc1bef3f5d59543de13fb
Melissa <melilssadelamerced@gmail.com>
2013-02-16 20:29:47 -0500

    added page 3 html

--- testweb.js
+++ testweb.js
@@ -4,6 +4,8 @@
 var http = require('http');
 var port = process.env.PORT || 5000;
 
+var url1 = '<a href="http://www.flickr.com/photos/mdelamerced/8458417011/" title="cpwinter.jpg 10 by mdelamerced, on Flickr"><img src="http://farm9.staticflickr.com/8520/8458417011_cbba27ee41_c.jpg" width="800" height="533" alt="cpwinter.jpg 10"></a>';
+
 var counter = 0;
 
 http.createServer(function (req, res) {
@@ -18,9 +20,13 @@ http.createServer(function (req, res) {
 
 	if (path == "/") {
 		res.end("Hello World. You are requestor # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
-
+		
+		
 	} else if (path == "/page2") {
-		res.end("This is really page 2. <a href='/'>Back.</a>\n"); // send response and close connection	
+		res.end("This is really page 2. <a href='/page3'>Next.</a>\n"); // send response and close connection	
+		
+	} else if (path =="/page3"){
+		res.end(url1+ "What are you still doing here? <a href='/'>Back.</a>\n");
 	}
 
 }).listen(port);
